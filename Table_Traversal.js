8e83702054a9fbd064819ca71c23ae5dd5679c8d
santhoshmk <connectwithsanthoshmk@gmail.com>
2025-04-05 03:42:47 +0530

    finished table traversal in console

---
+++ Table_Traversal.js
@@ -0,0 +1,22 @@
+var table = document.querySelector("#div_completeorders table");
+var totBuyPrice;
+var buy_investment = [];
+var sell_price = [];
+for (let i = 1; i < table.rows.length; i++) {
+  const row = table.rows[i];
+const rowArray = Array.from(row.cells).map(cell => cell.textContent);
+    if (rowArray[3].includes("BUY")){
+        buy_investment.push(Number(rowArray[7].replace(/,/g, '')));
+    }
+    else{
+        // console.log(Number(rowArray[4]),Number(rowArray[6]))
+        sell_price.push(Number(rowArray[4])*Number(rowArray[6]))
+    }
+  //console.log(`Row ${i}: ${Array.from(row.cells).map(cell => cell.textContent).join(" | ")}`);
+}
+var total_buy_investment=buy_investment.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+var total_sell_price=sell_price.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+console.log(buy_investment,total_buy_investment);
+console.log(sell_price,total_sell_price);
+console.log(total_sell_price-total_buy_investment);
+
--- scripts/content.js
+++ scripts/content.js
@@ -1,7 +1,8 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.action === 'getData') {
     // For example, retrieve some data from the page
-    document.querySelector("#lnk_completedorders").click();
+    console.log("need to unccoomment the below line in production")
+    // document.querySelector("#lnk_completedorders").click();
     let tb = document.evaluate('//div[@id="div_completeorders"]//table', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.outerHTML;
     // Send the data back to the popup
     sendResponse({ data: tb });
